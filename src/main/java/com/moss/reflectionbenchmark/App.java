8dc3be03754eec2ba7b2e1cf6dc813166447f5ee
Marcin Ossowski <mossowski2@p109-11.labpk.inf.ug.edu.pl>
2016-03-03 16:28:34 +0100

    Add maven

---
+++ src/main/java/com/moss/reflectionbenchmark/App.java
@@ -0,0 +1,13 @@
+package com.moss.reflectionbenchmark;
+
+/**
+ * Reflection Benchmark
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Reflection!" );
+    }
+}
